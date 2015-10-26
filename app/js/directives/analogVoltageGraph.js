ea2db4fbf659909f48fa73e7242bc5927840a8d5
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-10-26 16:48:15 -0500

    Add attributes for voltage directive

--- app/js/directives/analogVoltageGraph.js
+++ app/js/directives/analogVoltageGraph.js
@@ -12,12 +12,15 @@ function analogVoltageGraph(ApiService) {
   return {
     restrict: 'E',
     templateUrl: 'analogVoltageGraph.html',
-    scope: {},
+    scope: {
+      deviceCik: '@',
+      dataportRid: '@'
+    },
     link: function(scope, element, attrs) {
-      ApiService.getTimeSeries('345f025456fb4b9a5e120bb12a33d8245a8d0d1c', 'cdca21552437ae4349c8f003be6bcecdf5ab2ef9', 100, null, function (data) {
+      ApiService.getTimeSeries(scope.deviceCik, scope.dataportRid, 50, null, function (data) {
         scope.moment  = moment;
         scope.series = ['Analog Voltage'];
-        scope.labels = _.map(_.chunk(data, 5), function (chunk) { return moment.unix(chunk[0][0]).fromNow(); });
+        scope.labels = _.map(data, function (element) { return moment.unix(element[0]).fromNow(); });
         scope.data = [
           _.map(data, function (element) { return element[1]; })
         ];
