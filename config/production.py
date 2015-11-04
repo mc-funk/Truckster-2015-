117036284a121242c3512c27577593f611678a7f
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-11-04 15:05:20 -0600

    Update default token

--- config/production.py
+++ config/production.py
@@ -3,6 +3,6 @@ import time
 
 APP_CIK = os.getenv('APP_ROOT', '36aa36b52277b2273b4fcf1a3fba8e1c61255702')
 API_URL = 'https://api.exohack.io'
-API_KEY = os.getenv('JWTTOKEN', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiZXhhbXBsZSIsImRvbWFpbiI6Imh0dHBzOi8vZXhvaGFjay5pbyJ9.LdvbgB5-_kRbLCtYBlIxcbiX-N7UBAGvK5NfQclk_58')
+API_KEY = os.getenv('JWTTOKEN', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkb21haW4iOiJodHRwczovL2V4b2hhY2suaW8iLCJleHAiOjE0NDYyMjUyMDMsImlhdCI6MTQ0NjIxODAwMywibmFtZSI6ImV4YW1wbGUiLCJuYmYiOjE0NDYyMTgwMDJ9.R8pInT-85tAkcHJ0hcdo2hA3n1pZWOID4xwMFm-5lgg')
 API_WEBSOCKETS_URL = 'wss://api.exohack.io/socket'
 CACHE_BUSTER = int(time.time())
