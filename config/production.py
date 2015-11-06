df06adc3809d784a261d571fe88be79adb5ebc93
Michelle C. Funk <michellefunk@exosite.com>
2015-11-06 16:24:20 -0600

    add home.html

--- config/production.py
+++ config/production.py
@@ -1,8 +1,8 @@
 import os
 import time
 
-APP_CIK = os.getenv('APP_ROOT', '36aa36b52277b2273b4fcf1a3fba8e1c61255702')
+APP_CIK = os.getenv('APP_ROOT', '830fee687e5099bc2295085836c1a9371b671f27')
 API_URL = 'https://api.exohack.io'
-API_KEY = os.getenv('JWTTOKEN', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkb21haW4iOiJodHRwczovL2V4b2hhY2suaW8iLCJleHAiOjE0NDYyMjUyMDMsImlhdCI6MTQ0NjIxODAwMywibmFtZSI6ImV4YW1wbGUiLCJuYmYiOjE0NDYyMTgwMDJ9.R8pInT-85tAkcHJ0hcdo2hA3n1pZWOID4xwMFm-5lgg')
+API_KEY = os.getenv('JWTTOKEN', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoidHJ1Y2tzdGVyIiwiZG9tYWluIjoiaHR0cHM6Ly9leG9oYWNrLmlvIn0.rXWJKdtE0RXuHtrF2taeKRNl0SGQQ4zSKq5-2rEakyM')
 API_WEBSOCKETS_URL = 'wss://api.exohack.io/socket'
 CACHE_BUSTER = int(time.time())
