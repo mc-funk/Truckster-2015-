41445235d263bb1a9f403972ad1e57659b83a1ec
Will Charlton <willcharlton@exosite.com>
2015-11-06 16:29:41 -0600

    Added enpoints for truckster. Added condition to not use the angular build.

--- app.py
+++ app.py
@@ -2,14 +2,27 @@ import os
 import sys
 from flask import Flask, render_template, current_app
 import jinja2
-app = Flask(__name__, static_folder='build', static_path='')
+app = Flask(__name__,
+            # template_folder='truckster_web/templates',
+            static_folder='build',
+            static_path=''
+)
+
+BYPASSING_ANGULAR_FRONT_END = True
 
 # Override default location of templates
-my_loader = jinja2.ChoiceLoader([
-        app.jinja_loader,
-        jinja2.FileSystemLoader(['build'])
-    ])
-app.jinja_loader = my_loader
+if BYPASSING_ANGULAR_FRONT_END:
+    my_loader = jinja2.ChoiceLoader([
+            app.jinja_loader,
+            jinja2.FileSystemLoader(['app'])
+        ])
+    app.jinja_loader = my_loader
+else:
+    my_loader = jinja2.ChoiceLoader([
+            app.jinja_loader,
+            jinja2.FileSystemLoader(['build'])
+        ])
+    app.jinja_loader = my_loader
 
 # Load configuration
 app.config.from_object('config.production')
@@ -19,6 +32,18 @@ if app.debug:
 @app.route('/')
 def index():
     return render_template('index.html', **get_context())
+
+@app.route('/truckster')
+def truckster():
+    return render_template('truckster_web/templates/index.html', **get_context())
+
+@app.route('/trucks')
+def trucks():
+    return render_template('truckster_web/templates/index.html', **get_context())
+
+@app.route('/signin')
+def signin():
+    return render_template('truckster_web/templates/index.html', **get_context())
 
 @app.errorhandler(404)
 def not_found(error):
