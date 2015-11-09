d7d41ee81d8f39e4aae08f72262061dafc119a25
Michelle C. Funk <michellefunk@exosite.com>
2015-11-09 14:13:36 -0600

    Add auto update functionality using Firebase API

--- truckster/scripts/populate-truckmap.js
+++ truckster/scripts/populate-truckmap.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
       console.log("document ready");
 
+      var myFirebaseRef = new Firebase("https://exo-foodtruck.firebaseio.com/");
+
       var AllTruckData = jQuery.get("https://exo-foodtruck.firebaseio.com/trucks.json");
       var zoomLevel = 12;
       var exositeHomeGPS = [44.98014269999999,-93.28874739999999];
       // set up the map
-      map = new L.Map('map');
+      var map = new L.Map('map');
 
       // create the tile layer with correct attribution
       var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -14,6 +16,7 @@ $(document).ready(function(){
       var truckOnline;
       var outlineColor;
       var fillsColor;
+      var circles = {};
 
       function initmap() {
 
@@ -22,8 +25,8 @@ $(document).ready(function(){
           map.addLayer(osm);
         }
 
-      function add_food_trucks_to_map() {
-        $.each(AllTruckData.responseJSON, function(idx, val) {
+      function add_food_trucks_to_map(data) {
+        $.each(data, function(idx, val) {
           console.log("RID: ", idx, "Food Truck: ", val['name']);
           console.log("Val:", val);
           truckOnline = val['status']['online'];
@@ -35,7 +38,7 @@ $(document).ready(function(){
             fillsColor = '#222222';
           }
 
-          var circle = L.circle(  [ val['status']['gps']['lat'],
+          circles[val['name']] = L.circle(  [ val['status']['gps']['lat'],
                                     val['status']['gps']['long']
                                   ],
                        150, {
@@ -45,13 +48,21 @@ $(document).ready(function(){
           }).addTo(map);
 
           popupContent = '<p>' + val['name'] +'<br />'+ '<button type="button" class="addtruck btn btn-primary btn-sm">Stalk this Truck!</button></p>';
-          circle.bindPopup(popupContent);
+          circles[val['name']].bindPopup(popupContent);
             // .setContent('<p>' + val['name'] +'<br />'+ '<button type="button">' + Stalk this Truck! + '</button></p>');
         });
       }
 
       initmap();
-      jQuery.when(AllTruckData).done(add_food_trucks_to_map)
+      jQuery.when(AllTruckData).done(function() {
+        add_food_trucks_to_map(AllTruckData.responseJSON);
+      });
+      myFirebaseRef.on('value', function(dataSnapshot) {
+        console.log("Map refreshing - new data");+
+        console.log(dataSnapshot.val()['trucks']);
+        map.removeLayer(circles);
+        add_food_trucks_to_map(dataSnapshot.val()['trucks']);
+      });
 
 
       // console.log(AllTruckData.responseJSON)
