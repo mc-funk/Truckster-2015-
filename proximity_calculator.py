2be5e64354e41a4194fbb3ff9ed2f162e1140114
Will Charlton <willcharlton@exosite.com>
2015-11-09 16:24:38 -0600

    Starting to add proximity notification logic.

--- proximity_calculator.py
+++ proximity_calculator.py
@@ -4,7 +4,7 @@ from time import sleep
 import requests, json
 
 FB_URL = "https://exo-foodtruck.firebaseio.com/trucks.json"
-FB_SEND_MSG_FMT_URL = "https://exo-foodtruck.firebaseio.com/trucks.json"
+FB_SEND_MSG_FMT_URL = "https://exo-foodtruck.firebaseio.com/trucks/{rid}/subscribers/0/.json"
 def main():
 	while True:
 		all_trucks = requests.get(FB_URL).json()
@@ -21,6 +21,10 @@ def main():
 						"Subscriber: ", subscriberLocation,
 						"Haversine Dist: ", proximity
 				)
+				if proximity >= blob['subscribers']['addresses'][0]['geofence_data']:
+					# set flag to tell 1P to send notification
+					pass
+
 
 			else:
 				print("No subscribers for ", blob['name'])
