982f5d7eabac8cd25b9be4c050f82dd10ecad72d
Michelle C. Funk <michellefunk@exosite.com>
2015-11-10 09:22:54 -0600

    Integrate Will's changes to popup

--- truckster/scripts/populate-truckmap_temp.js
+++ truckster/scripts/populate-truckmap_temp.js
@@ -29,16 +29,16 @@ $(document).ready(function(){
       function add_food_trucks_to_map(data) {
         $.each(data, function(idx, val) {
           console.log("RID: ", idx, "Food Truck: ", val['name']);
-          console.log("Val name:", val['name']);
           truckOnline = val['status']['online'];
+
           if (truckOnline == true) {
             fillsColor = "#B83C30";
             outlineColor = "#AC7143"
-            openStatus = "Currently open here!";
+            openStatus = "Currently open here!"
           } else {
             outlineColor = '#000000';
             fillsColor = '#222222';
-            openStatus = "Closed, last seen here";
+            openStatus = "Closed, last seen here"
           }
 
           circles[val['name']] = L.circle(  [ val['status']['gps']['lat'],
@@ -50,25 +50,9 @@ $(document).ready(function(){
             fillOpacity: 0.8
           }).addTo(map);
 
+          //console.log('openStatus:', openStatus)
 
-          var subscribe_form = "<p>" + val['name'] + "</p><br />\
-                                  <form action='/truckster/' method='post'>\
-                                    <input type='hidden' name='rid' value="+idx+">\
-                                    <input type='hidden' name='truck_name' value="+val['name']+">\
-                                      Name: <input type='text' name='name' value=''>\
-                                      <br>\
-                                      Preferred contact method: <input type='radio' name='contact_method' value='sms' checked>SMS\
-                                        <input type='radio' name='contact_method' value='email'>Email\
-                                      <br>\
-                                      Contact info: <input type='text' name='contact' value=''>\
-                                      <br>\
-                                      Notify when within 1KM of address: <input type='text' name='address' value=''>\
-                                      <br>\
-                                      <input class='addtruck btn btn-primary btn-sm' type='submit' value='Stalk this truck!'>\
-                                  </form>"
-
-          //popupContent = '<p>' + val['name'] +'<br /><em>'+ openStatus + '</em><br /><button type="button" class="addtruck btn btn-primary btn-sm">Stalk this Truck!</button></p>';
-          popupContent = subscribe_form;
+          popupContent = '<p>' + val['name'] +'<br /><em>'+ openStatus + '</em><br /><button type="button" class="addtruck btn btn-primary btn-sm">Stalk this Truck!</button></p>';
           circles[val['name']].bindPopup(popupContent);
             // .setContent('<p>' + val['name'] +'<br />'+ '<button type="button">' + Stalk this Truck! + '</button></p>');
         });
@@ -79,16 +63,16 @@ $(document).ready(function(){
         add_food_trucks_to_map(AllTruckData.responseJSON);
       });
       myFirebaseRef.on('value', function(dataSnapshot) {
-        console.log("Map refreshing - new data");+
-        console.log(dataSnapshot.val()['trucks']);
-        console.log('circles: ', circles);
+        console.log("Map refreshing - new data");
+        //console.log(dataSnapshot.val()['trucks']);
+        //console.log('circles: ', circles);
+        var updatedTruckData = dataSnapshot.val()['trucks'];
         for (var drawnCircle in circles) {
-          console.log("drawnCircle", drawnCircle);
+          //console.log("drawnCircle", drawnCircle);
           map.removeLayer(circles[drawnCircle]);
         }
-        add_food_trucks_to_map(dataSnapshot.val()['trucks']);
+        add_food_trucks_to_map(updatedTruckData);
       });
-
 
       // console.log(AllTruckData.responseJSON)
       // $.each(AllTruckData.responseJSON, function(idx, val) {
