0be8f622d511d8ba211ec6bbae25107b58ea55bb
Michelle C. Funk <michellefunk@exosite.com>
2015-11-10 10:36:19 -0600

    Subscribe to ... truck text change

--- truckster/scripts/populate-truckmap.js
+++ truckster/scripts/populate-truckmap.js
@@ -53,7 +53,7 @@ $(document).ready(function(){
 
 
           function subscribe_form(name, id) {
-            return "<h4>" + name + "</h4>\
+            return "<h4>Subscribe to " + name + "</h4>\
                                   <form action='/truckster/' method='post'>\
                                   <input type='hidden' name='rid' value="+id+">\
                                   <input type='hidden' name='truck_name' value="+name+">\
